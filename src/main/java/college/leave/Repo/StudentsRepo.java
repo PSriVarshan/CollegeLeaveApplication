3ad34f34738bba30666089ff04e43dcff5c1267e
PSriVarshan <psvvarshan@gmail.com>
2025-04-29 13:45:56 +0530

    Added custom query in LeaveRepo

--- src/main/java/college/leave/Repo/StudentsRepo.java
+++ src/main/java/college/leave/Repo/StudentsRepo.java
@@ -2,7 +2,9 @@ package college.leave.Repo;
 
 import college.leave.Model.StudentsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StudentsRepo extends JpaRepository<StudentsEntity, Long> {
 
 }
