c8b8720e588a55e244625d4105503a51aac360a4
PSriVarshan <psvvarshan@gmail.com>
2025-04-29 11:02:04 +0530

    ApprovalStatusAdded

--- src/main/java/college/leave/Model/Enums/ApprovalStatus.java
+++ src/main/java/college/leave/Model/Enums/ApprovalStatus.java
@@ -1,7 +1,22 @@
 package college.leave.Model.Enums;
 
+/**
+ * Represents the approval status of a leave request.
+ * This enum defines the possible states of a leave application.
+ */
 public enum ApprovalStatus {
+    /**
+     * Indicates that the leave request has been approved.
+     */
     APPROVED,
+
+    /**
+     * Indicates that the leave request is pending review.
+     */
     PENDING,
+
+    /**
+     * Indicates that the leave request has been rejected.
+     */
     REJECTED
 }
