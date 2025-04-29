02afb89f7bf66ad88a009a6602bac8f25a525909
PSriVarshan <psvvarshan@gmail.com>
2025-04-29 10:06:56 +0530

    first commit

---
+++ src/test/java/college/leave/LeaveApplicationTests.java
@@ -0,0 +1,13 @@
+package college.leave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
