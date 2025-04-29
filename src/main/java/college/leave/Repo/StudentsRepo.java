95f2029265f430a77825e06266ed95af371554b4
annblessy <annblessy2004@gmail.com>
2025-04-29 13:48:13 +0530

    Updated StudenService, DeptService, StudentRepo

--- src/main/java/college/leave/Repo/StudentsRepo.java
+++ src/main/java/college/leave/Repo/StudentsRepo.java
@@ -1,8 +1,21 @@
 package college.leave.Repo;
 
 import college.leave.Model.StudentsEntity;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface StudentsRepo extends JpaRepository<StudentsEntity, Long> {
+    @Modifying
+    @Transactional
+    @Query("UPDATE students SET studentName=:newName, email=:newEmail, phoneNo=:newContact WHERE studentId=:sid")
+    void updateStudentDetailsInLeave(
+            @Param("newName") String newName,
+            @Param("newEmail") String newEmail,
+            @Param("newContact") Long newContact,
+            @Param("sid") Long sid
+    );
 
 }
