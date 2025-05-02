221dc84a7f9ce49218f8971dd986354333ee592c
PSriVarshan <psvvarshan@gmail.com>
2025-05-02 10:35:36 +0530

    DeptController added

--- src/main/java/college/leave/Controller/DeptController.java
+++ src/main/java/college/leave/Controller/DeptController.java
@@ -1,4 +1,10 @@
 package college.leave.Controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping
+@RestController
 public class DeptController {
+
 }
