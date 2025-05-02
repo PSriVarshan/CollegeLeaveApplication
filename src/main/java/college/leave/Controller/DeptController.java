f1a1cc9deba142d1a3cabacd650f7d2ead8c383a
annblessy <annblessy2004@gmail.com>
2025-05-02 10:42:22 +0530

    Updated DeptController

--- src/main/java/college/leave/Controller/DeptController.java
+++ src/main/java/college/leave/Controller/DeptController.java
@@ -1,10 +1,19 @@
 package college.leave.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import college.leave.Model.DeptEntity;
+import college.leave.Service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping
+@RequestMapping("/dept/api")
 @RestController
 public class DeptController {
+    @Autowired
+    private DepartmentService deptService;
 
+    @PostMapping("/addDept")
+    public DeptEntity addDept(@RequestBody DeptEntity dept){
+        return deptService.addDepartment(dept);
+    }
 }
